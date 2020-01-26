d14bcba1d0555af7326964036ab21f479c0f2d98
Liam Galvin <liam@liam-galvin.co.uk>
2020-01-26 12:31:36 +0000

    v0.19.0

--- tfsec.rb
+++ tfsec.rb
@@ -1,13 +1,13 @@
 class Tfsec < Formula
   desc "Static analysis powered security scanner for your terraform code"
   homepage "https://github.com/liamg/tfsec"
-  url "https://github.com/liamg/tfsec/archive/v0.16.0.tar.gz"
-  sha256 "b2b0da6cc753b3e0c6cad4e7b5ec83648016d7ee5da951a8e5bd4641da4475a6"
+  url "https://github.com/liamg/tfsec/archive/v0.19.0.tar.gz"
+  sha256 "3f843df9d602749365638588dff08f259ec58cf018fb7db553e3f42fe4865547"
 
   depends_on "go" => :build
 
   def install
-	  system "scripts/install.sh", "v0.16.0"
+	  system "scripts/install.sh", "v0.19.0"
     bin.install "tfsec" => "tfsec"
   end
 
