3b67a00dc7b8647ec32ad10f29afbf8f31243512
Christian Eager <christian@turo.com>
2020-09-22 18:49:19 -0500

    Append missing " to sha256

--- tfsec.rb
+++ tfsec.rb
@@ -2,7 +2,7 @@ class Tfsec < Formula
   desc "Static analysis powered security scanner for your terraform code"
   homepage "https://github.com/liamg/tfsec"
   url "https://github.com/liamg/tfsec/archive/v0.27.0.tar.gz"
-  sha256 "95cfba8fafe142c79204928896c197cf6902e75d4c7d76b2662e784c227180fb
+  sha256 "95cfba8fafe142c79204928896c197cf6902e75d4c7d76b2662e784c227180fb"
   depends_on "go" => :build
 
   def install
